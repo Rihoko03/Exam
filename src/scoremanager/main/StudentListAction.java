50d3cbe5cf70f831aa6471b5a52e96629b890196
oohara <oohara@172.16.31.76>
2024-04-12 15:18:33 +0900

    2024/04/12(金)

---
+++ src/scoremanager/main/StudentListAction.java
@@ -0,0 +1,51 @@
+package scoremanager.main;
+import java.time.LocalDate;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Student;
+import bean.Teacher;
+import dao.StudentDao;
+import tool.Action;
+
+public class StudentListAction extends Action{
+
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession();
+		Teacher teacher = (Teacher)session.getAttribute("user");
+
+		String entYearStr="";
+		String classNum="";
+		String isAttendsStr="";
+		int entYear =0;
+		boolean isAttend = false;
+		List<Student> students = null;
+		LocalDate todaysDate = LocalDate.now();
+		int year = todaysDate.getYear();
+		StudentDao sDao = new StudentDao();
+		ClassNumDao cNumDao = new ClassNumDao();
+		Map<String, String> errors = new HashMap<>();
+
+		entYearStr = req.getParameter("f1");
+		classNum = req.getParameter("f2");
+		isAttendStr = req.getParameter("f3");
+
+		List<String> list = cNumDao.filter(teacher.getSchool()));
+
+		if (entYear ! =0 && !classNum.equals("0")){
+			students = sDao.filter(teacher.getSchool(), entYear, classNum, isAttend);
+		} else if (enrYear != 0 && classNum.equals("0")){
+
+		}else if (enrYear != 0 && classNum.equals("0")){
+
+		}else if (enrYear != 0 && classNum.equals("0")){
+
+		}
+
+	}
+}
--- src/tool/FrontController.java
+++ src/tool/FrontController.java
@@ -2,6 +2,7 @@ package tool;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,15 +22,15 @@ public class FrontController extends HttpServlet{
 
 
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletExcetion, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	//TODO Auto-generated method stub
-		response.getWriter().append("Served at:").append(request.getContextPath());
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 
 
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletExcetion, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//TODO Auto-generated method stub
 		doGet(request, response);
 	}
